eee7243b509121e0c6659fc00c301a998c07b945
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-10-08 03:19:43 +0300

    2.19

--- text_mining.py
+++ text_mining.py
@@ -1,5 +1,7 @@
 from sklearn.datasets import fetch_20newsgroups
 
-categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
-twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
+# Tutorial example 4, but in the assignment we are using 20
+# categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+
+twenty_train = fetch_20newsgroups(subset='train', categories=None, shuffle=True, random_state=42)
 
